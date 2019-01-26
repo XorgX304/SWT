8ee705bf391e99a2027d6380ced965230a501c0f
Tebreca <ewan.arends10@gmail.com>
2019-01-26 17:11:11 +0100

    start ssl and add pages

--- java/src/main/java/nl/thedarkarmy/swt/main/Main.java
+++ java/src/main/java/nl/thedarkarmy/swt/main/Main.java
@@ -1,14 +1,62 @@
 package nl.thedarkarmy.swt.main;
 
 import net.freeutils.httpserver.HTTPServer;
+import nl.thedarkarmy.swt.pages.Pages;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
 
     private static HTTPServer httpServer;
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         httpServer = new HTTPServer(443);
+
+        doSSL();
+
         httpServer.start();
+        httpServer.getVirtualHost(null).addContexts(new Pages());
     }
 
+    private static void doSSL() {
+        try{
+            IO.touch();
+        } catch (IOException e){
+            e.printStackTrace();
+            return;
+        }
+        httpServer.
+
+    }
+
+
+    private static class IO{
+        public static XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat());
+
+        public static void touch() throws IOException {
+            File f = new File("/sslconfig.xml");
+            if(!f.exists()){
+                Element root = new Element("ssl");
+                Document document = new Document(root);
+                Element file = new Element("file");
+                file.setAttribute("location", "./key.jks");
+                root.addContent(file);
+                Element password = new Element("password");
+                password.setText("password");
+                root.addContent(password);
+                f.createNewFile();
+                System.out.println(f.getAbsolutePath());
+                FileWriter fileWriter = new FileWriter(f);
+                xmlOutputter.output(document, fileWriter);
+            }
+        }
+
+    }
 }
---
+++ java/src/main/java/nl/thedarkarmy/swt/pages/Pages.java
@@ -0,0 +1,17 @@
+package nl.thedarkarmy.swt.pages;
+
+import net.freeutils.httpserver.HTTPServer.*;
+
+import java.io.IOException;
+
+public class Pages {
+
+    @Context("/example")
+    public int example(Request req, Response resp) throws IOException {
+        resp.getHeaders().add("Content-Type", "text/plain");
+        resp.send(200, "Hello, World!");
+        //code
+        return 0;
+    }
+
+}
